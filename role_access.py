ea7befc5ab29afa0d0bf3c3401afc0e4e737ee87
vagolisakshi0-lgtm <vagolisakshi0@gmail.com>
2026-09-16 18:00:24 +0530

    Add files via upload

---
+++ role_access.py
@@ -0,0 +1,11 @@
+ratrole=input("enter a role:")
+if role is"admin":
+    print("admin=checking weather the accont is active or not")
+elif role is"manager":
+    print("grant report access")
+else:
+     print("not grant")
+     active="get_full_accessed"
+     inactive="blocked"
+if role is"emloyee":
+    print("grant basic access only")
