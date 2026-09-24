eaf24dc59ab83ac33bffe7179030730b0fe5d16b
vagolisakshi0-lgtm <vagolisakshi0@gmail.com>
2026-09-24 15:37:22 +0530

    Add files via upload

---
+++ cleaning and searching.py
@@ -0,0 +1,16 @@
+data=""
+print("intern data\n".strip())
+batch=""
+print("Batch-07".replace("Batch","Group"))
+DDI=""
+print("welcome to DDI".find("DDI"))
+count=""
+print("aaa bbb aaa".count("aaa"))
+
+
+names="Asha,Ravi,Meera"
+names.split(",")
+print(names)
+sentence="Learn python daily"
+sentence.split(",")
+print(sentence)
